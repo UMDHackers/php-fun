353d71433fc666ad126ec9d7c186a3b295ccdc6c
Parth Desai <parthdesai95@gmail.com>
2015-11-10 04:34:25 +0000

    Login features and some password

--- create_user.php
+++ create_user.php
@@ -1,1 +1,35 @@
-<h1>Hello</h1>
+<h3>Sign Up</h3>
+<form method = "post" action = "<?php echo $_SERVER['PHP_SELF'];?>" >
+    <p>Enter Username: <input type="text" name="username"/></p>
+    <p>Enter Password: <input type="password" name="password1"/></p>
+    <p>Re-enter Password: <input type="password" name="password2"/></p>
+    <p><?php echo $password_error; ?></p>
+    <input type="submit" value="Submit"/>
+</form>
+
+<!--
+<script type="text/javascript">
+  function correctInformation(form) {
+    if(form.password1.value != form.password2.value) {
+        alert("Mismatched passwords");
+    } 
+      
+  }
+</script>
+-->
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        //Render information
+        $name = $_POST['username'];
+        $password1 = $_POST['password1'];
+        $password2 = $_POST['password2'];
+        if ($password1 != $password2) {
+            echo "Passwords mismatch";
+        }
+        else {
+            echo ""
+        }
+        
+    } 
+        
+?>
--- index.php
+++ index.php
@@ -1,7 +1,27 @@
-<h1>Hello world</h1>
-<h3>Doing some quick php testing!</h3>
+<html>
+<body>
 
-<?php
-    echo "Here is a random numberrv\n";
-    echo rand(1, 100000)."\n";
-?>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  <p>Username: <input type="text" name="uname"></p>
+  <p>Password: <input type="password" name="pname"></p>
+  <input type="submit" value="Login in">
+</form>
+<button><a id = "new_account" href = "create_user.php">Create new account</a></button>
+ <?php
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        //Render information
+        $name = $_POST['uname'];
+        $password = $_POST['pname'];
+        if(empty($name) || empty($password)) {
+            echo "Missing infromation";
+        } else {
+            echo "Username: ".$name;
+            echo "<br>";
+            echo "Password: ".$password;
+        }
+    } 
+ ?>
+
+
+</body>
+</html>
