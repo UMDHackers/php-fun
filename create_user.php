d61950f6fc0b7e49c097ce122bc89030913173bb
Parth Desai <parthdesai95@gmail.com>
2016-01-14 22:14:44 -0500

    Update create_user.php

--- create_user.php
+++ create_user.php
@@ -1,7 +1,7 @@
 <h3>Sign Up</h3>
 <form method = "post" action = "<?php echo $_SERVER['PHP_SELF'];?>" >
 	<!--<p>Username: &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <input type = "text" name = "name"/></p>-->
-	<p>Enter Username: <input type="text" name="username"/></p>
+	  <p>Enter Username: <input type="text" name="username"/></p>
     <p>Enter Password:  <input type="password" name="password1"/></p>
     <p>Re-enter Password: <input type="password" name="password2"/></p>
     <input type="submit" value="Submit"/>
@@ -20,33 +20,37 @@
         }
         else {
             // Create connection
-				$conn = new mysqli("XXXX","XXX", "XXX", "XXXX");
+				$conn = new mysqli("","", "", "");
 
 				// Check connection
 				if ($conn->connect_error) {
 					die("Connection failed: " . $conn->connect_error);
 				}
 				//echo "Connected successfully"."<br>";
-				$present = "SELECT username FROM users WHERE user_name = '$user'";
+				$present = "SELECT * FROM users WHERE user_name = '$user'";
+				//echo $present;
 				$result = $conn->query($present);
-
+				//echo "Works?";
+				//echo $result;
 				//Check if the user doesn't exist
-				if(empty($result->fetch_assoc())){
-					$file_system = $username."_whiteborad" ;
-					$sql = "INSERT INTO users (username, password, file_location) VALUES ('$name', '$password1', '$file_system')";
+				//echo $result->fetch_assoc();
+
+				if($result->num_rows === 0){
+					$file_system = $user."_whiteborad" ;
+					$sql = "INSERT INTO users (user_name, password, file_location) VALUES ('$user', '$password1', '$file_system')";
 					if ($conn->query($sql) === TRUE) {
 						//Create dafult JPG and get the JPG image and place that in images table
 						//Create image
 						$image = imagecreatetruecolor(950, 400);
-						$white = imagecolorallocate($im, 255, 255, 255);
-						imagefill($image, 0, 0, $white);
+						$white = imagecolorallocate($image, 255, 255, 255);
+						imagefill($image, 255, 255, 255);
 
 						//save to file system, my local drive
 						imagejpeg($image, 'drawing/'.$file_system);
 
 						header("Location: profile.php?username=$user");
 					} else {
-						echo "Error: ". $sqlre . "<br>" . $conn->error;
+						echo "Error: ". $conn->error;
 					}
 				} else {
 					echo "username already taken"."<br>";
